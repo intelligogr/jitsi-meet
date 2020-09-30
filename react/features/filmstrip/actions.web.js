ae3f83fa027e54f5addf02e21a3ceb54ee82c4ec
Erik Demaine <edemaine@mit.edu>
2020-09-30 17:03:00 -0400

    Optimally pack tiles in tile view
    Add TILE_VIEW_MAX_ROWS option to customize maximum number of rows
    separately from maximum number of columns, now that layout may not be
    square.

--- react/features/filmstrip/actions.web.js
+++ react/features/filmstrip/actions.web.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { CHAT_SIZE } from '../chat/constants';
+import { getMaxColumnCount, getMaxRowCount } from '../video-layout';
 
 import { SET_HORIZONTAL_VIEW_DIMENSIONS, SET_TILE_VIEW_DIMENSIONS } from './actionTypes';
 import { calculateThumbnailSizeForHorizontalView, calculateThumbnailSizeForTileView } from './functions';
@@ -11,36 +12,72 @@ import { calculateThumbnailSizeForHorizontalView, calculateThumbnailSizeForTileV
 const TILE_VIEW_SIDE_MARGINS = 10 * 2;
 
 /**
- * Sets the dimensions of the tile view grid.
+ * Sets the dimensions of the tile view grid.  Tries layouts for each
+ * number of columns ranging from 1 to the maximum (up to 5), preferring
+ * layouts that show more tiles, and subject to this, preferring layouts
+ * with larger tiles, optimizing the available space.
  *
- * @param {Object} dimensions - Whether the filmstrip is visible.
- * @param {Object} windowSize - The size of the window.
- * @param {boolean} isChatOpen - Whether the chat panel is displayed, in
- * order to properly compute the tile view size.
+ * Should be called whenever the number of participants or the available
+ * space (including window size and whether the chat sidebar is displayed)
+ * changes.
+ *
+ * @param {Object} state - The Redux state object.
  * @returns {{
  *     type: SET_TILE_VIEW_DIMENSIONS,
  *     dimensions: Object
  * }}
  */
-export function setTileViewDimensions(dimensions: Object, windowSize: Object, isChatOpen: boolean) {
-    const { clientWidth, clientHeight } = windowSize;
+export function setTileViewDimensions(state: Object) {
+    // Compute available window size, discounting chat sidebar.
+    const { clientHeight, clientWidth } = state['features/base/responsive-ui'];
+    const { isOpen } = state['features/chat'];
     let widthToUse = clientWidth;
-
-    if (isChatOpen) {
+    if (isOpen) {
         widthToUse -= CHAT_SIZE;
     }
 
-    const thumbnailSize = calculateThumbnailSizeForTileView({
-        ...dimensions,
-        clientWidth: widthToUse,
-        clientHeight
-    });
-    const filmstripWidth = dimensions.columns * (TILE_VIEW_SIDE_MARGINS + thumbnailSize.width);
+    // When in tile view mode, we must discount ourselves (the local participant) because our
+    // tile is not visible.
+    const { iAmRecorder } = state['features/base/config'];
+    const numberOfParticipants = state['features/base/participants'].length - (iAmRecorder ? 1 : 0);
+
+    const maxColumns = getMaxColumnCount();
+    const maxRows = getMaxRowCount();
+
+    // Try each possible number of columns.
+    let thumbnailSize, columns, visibleRows; // current best layout
+    for (let tryColumns = 1; tryColumns <= maxColumns; tryColumns++) {
+        const tryRows = Math.ceil(numberOfParticipants / tryColumns);
+        const tryVisibleRows = Math.min(maxRows, tryRows);
+        const tryThumbnailSize = calculateThumbnailSizeForTileView({
+            columns: tryColumns,
+            visibleRows: tryVisibleRows,
+            clientWidth: widthToUse,
+            clientHeight
+        });
+        if (tryColumns === 1 ||
+            // Prefer layouts showing more participants/tiles. Because we
+            // try layouts in increasing order by columns, and rows is
+            // limited by a maximum, this preference will take priority.
+            (visibleRows * columns < numberOfParticipants &&
+             visibleRows * columns < tryVisibleRows * tryColumns) ||
+            // Prefer layouts with larger tile size (width).
+            thumbnailSize.width < tryThumbnailSize.width) {
+            thumbnailSize = tryThumbnailSize;
+            columns = tryColumns;
+            visibleRows = tryVisibleRows;
+        }
+    }
+
+    const filmstripWidth = columns * (TILE_VIEW_SIDE_MARGINS + thumbnailSize.width);
 
     return {
         type: SET_TILE_VIEW_DIMENSIONS,
         dimensions: {
-            gridDimensions: dimensions,
+            gridDimensions: {
+                columns,
+                visibleRows,
+            },
             thumbnailSize,
             filmstripWidth
         }
